e1756fc8c44f23a9e9f842a5bb436992f32594f5
serendatapy <arvaldivia@gmail.com>
2019-11-10 00:37:08 +0100

    added homepage work done - starting the custom archive page for case studies

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -19,8 +19,34 @@ get_header(); ?>
 				<?php the_content(); ?>
 				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+		</div><!-- .main-content site content -->
+
+
+	</div><!-- #primary homepage-->
+
+	<section class="featured-work">
+ 		<div class="site-content">
+				 <h4>Featured Work</h4>
+
+				<ul class="homepage-featured-work">
+				 <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+					 <?php while (have_posts()) : the_post(); 
+					 $image_1 = get_field("image_1");
+					 $size = "medium";
+					?>
+					<li class="individual-featured-work">
+					 	<figure>
+					 		<?php echo wp_get_attachment_image($image_1,$size); ?>
+					 	</figure>
+
+					  	<h3><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a></h3>
+					 </li>
+					 <?php endwhile; //end of loop. ?> 
+				 	<?php wp_reset_query(); //resets the altered query back to the original ?>
+				</ul>
+ 		</div>
+	</section>
+
 	<section class="recent-posts">
  		<div class="site-content">
 			 <div class="blog-post">
