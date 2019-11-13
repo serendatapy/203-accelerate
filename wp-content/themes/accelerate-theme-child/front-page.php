0ab26654411f6afee8ebf23f1c1b80aa7a95601b
serendatapy <arvaldivia@gmail.com>
2019-11-13 14:19:32 +0100

    Made a mess and restored from backup because plugin isn't working, doesn't seem to have fixed it though

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the homepage
+ * The template for displaying the homepage CHILD THEME
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
