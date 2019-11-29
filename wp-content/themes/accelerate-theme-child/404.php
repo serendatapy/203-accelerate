b43391124a5d404e2cdcc8d85ecc31f55ee47db0
serendatapy <arvaldivia@gmail.com>
2019-11-29 22:20:04 +0100

    general update, creating 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying 404 page
+ *
+ * Template Name: Error page
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+	
+	<div id="primary" class="site-content">
+		<div id="content" role="main">	
+			<h1>THIS ISN'T WORKING OUT, BUT IT'S NOT YOU, IT'S YOUR MUM </h1>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
